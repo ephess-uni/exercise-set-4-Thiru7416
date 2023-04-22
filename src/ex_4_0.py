ad75048667cd3cb4c859d5d18a4bdf007490d9f2
Thiru7416 <128247232+Thiru7416@users.noreply.github.com>
2023-04-21 22:59:56 -0500

    Update ex_4_0.py

--- src/ex_4_0.py
+++ src/ex_4_0.py
@@ -1,4 +1,3 @@
-""" ex_4_0.py """
 try:
     from src.util import get_data_file_path
 except ImportError:
@@ -10,10 +9,12 @@ FILENAME = get_data_file_path('messages.log')
 
 
 def get_shutdown_events(logfile):
-    """
-    Your docstring here.  Replace the pass keyword below with your implementation
-    """
-    pass
+    shutdown_lines = []
+    with open(logfile, "r") as f:
+        for line in f:
+            if "Shutdown initiated" in line:
+                shutdown_lines.append(line.strip())
+    return shutdown_lines
 
 
 # >>>> The code below will call your function and print the results
